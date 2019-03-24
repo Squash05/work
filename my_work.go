7629da9c815de6f03a4b4ee3d432e03ae53b6b81
Squash <40328976+Squash05@users.noreply.github.com>
2019-03-24 10:47:18 +0200

    Update my_work.go

--- my_work.go
+++ my_work.go
@@ -11,14 +11,16 @@ import (
 	"log"
 	"sort"
 )
-// ReadFile takes the path to a file and reads the contents of this file
-func ReadFile(filename string) ([]byte, error){}
+// ReadFile takes the path to a file and assigns the content to releases
+func ReadFile(filename string) ([]byte, error){
+	var newFile = (os.Args[0])
+	var cmp = require('semver-compare');
+}
 
 // LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
-	var cmp = require('semver-compare');
 	var versionSlice []*semver.version
-        minVersion := semver.New("1.8.0")
+	minVersion := semver.New("1.8.0")
 	allReleases := make([]*semver.Version, len(releases), (cap(releases)+1)*2)
 	for i, release := range releases {
 		versionString := *release.TagName
@@ -28,10 +30,12 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 		allReleases[i] = semver.New(versionString)
 	}
 	versionSlice := LatestVersions(allReleases, minVersion)
+	
 	return versionSlice
 }
 
 func main() {
+	
 	client := github.NewClient(nil)
 	ctx := context.Background()
 	opt := &github.ListOptions{PerPage: 10}
@@ -41,20 +45,20 @@ func main() {
         os.Exit(1)
 	}
 	
-	releases,  _, err := ioutil.ReadFile(ctx, os.Args[1], opt)
-        if err != nil {
-              fmt.Println("Can't read file:", os.Args[1])
-              log.Fatal(err)
-	} else {
-              releases, _, err := client.repositories.ListReleases(ctx, "Kubernetes", "Kubernetes", opt)
-	      if err != nil {
-		          log.Fatal(err)
-	      }
-  }
-	
+	newFile,  _, err := ioutil.ReadFile(ctx, os.Args[1], opt)
+    if err != nil {
+        fmt.Println("Can't read file:", os.Args[1])
+        log.Fatal(err)
+	}
+	releases, _, err := client.repositories.ListReleases(ctx, "Kubernetes", "Kubernetes", opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 
 	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
-	fmt.Printf("latest versions of kubernetes/kubernetes: [%s]", console.log( releases.sort(cmp).join(' ')));
+	fmt.Printf("latest versions of %s: [%s]", newFile , console.log(os.Args[1].sort(cmp).join(' ')));
 }
+
 
 
